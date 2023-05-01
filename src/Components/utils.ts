d2e6e6ec503feb24a9b0e4a67111c290bf649ec5
buntin-Synthia-suse <mail@buntinjp.net>
2023-05-01 12:00:36 +0900

    scaling

--- src/Components/utils.ts
+++ src/Components/utils.ts
@@ -1,42 +1,42 @@
 import { ChannelInfo, DirectoryOutbound, Checked } from './types';
 
-const url = 'http://localhost:3000';
+const url = 'https://manga.buntin.xyz';
 
 const fetchChannelNames = async () => {
-  const name = await fetch(`${url}/channel`);
-  const info = (await name.json()) as ChannelInfo;
-  return info;
+    const name = await fetch(`${url}/channel`);
+    const info = (await name.json()) as ChannelInfo;
+    return info;
 };
 
 const fetchChannelChange = async (index: number) => {
-  const res = await fetch(`${url}/channel`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      index,
-    }),
-  });
-  const result = await res.json();
-  console.log('in fetchChannelChange', result);
-  return result as ChannelInfo;
+    const res = await fetch(`${url}/channel`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            index,
+        }),
+    });
+    const result = await res.json();
+    console.log('in fetchChannelChange', result);
+    return result as ChannelInfo;
 };
 
 const fetchDirectory = async () => {
-  const res = await fetch(`${url}/directory`);
-  const directory = (await res.json()) as DirectoryOutbound;
-  return directory;
+    const res = await fetch(`${url}/directory`);
+    const directory = (await res.json()) as DirectoryOutbound;
+    return directory;
 };
 
 const pushDirData = async (data: Checked[]) => {
-  fetch(`${url}/directory`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
+    fetch(`${url}/directory`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
 };
 
 export { fetchChannelNames, fetchChannelChange, fetchDirectory, pushDirData };
